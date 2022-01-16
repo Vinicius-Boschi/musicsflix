a77785c1becdeeaedf06359a5aa0d4a6bd2808b3
Vinicius Boschi <vinicius.yg@gmail.com>
2022-01-16 13:55:40 -0300

    Iniciando o js na página

--- artistas/c/coldplay/coldplay.js
+++ artistas/c/coldplay/coldplay.js
@@ -1,1 +1,14 @@
 // foto da música, nome, artista, link.
+
+let musics = [
+
+    { 
+        name: "My Universe",
+        image: "https://rollingstone.uol.com.br/media/_versions/coldplay-bts-my-universe-rproducao-divulgacao_widelg.jpg",
+        singer: "Coldplay",
+        album: "Music of the Spheres"
+    },
+
+]
+
+let artitsMusics = document.getElementById("artists__musics")
