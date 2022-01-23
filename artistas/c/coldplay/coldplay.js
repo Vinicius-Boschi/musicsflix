bc086453b6982fc46fad8a0549e03ebb14f9df2a
Vinicius Boschi <vinicius.yg@gmail.com>
2022-01-23 15:30:04 -0300

    Adicionando botão de voltar ao topo

--- artistas/c/coldplay/coldplay.js
+++ artistas/c/coldplay/coldplay.js
@@ -126,3 +126,18 @@ function load() {
 }
 
 load()
+
+let button = $('.btn');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    button.addClass('show');
+  } else {
+    button.removeClass('show');
+  }
+})
+
+button.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300')
+})
