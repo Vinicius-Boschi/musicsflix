eaa38426e12fc278b5f9f95c94424f9d2bc31fb3
Vinicius Boschi <vinicius.yg@gmail.com>
2022-01-25 21:04:36 -0300

    Adicionando nova página

---
+++ artistas/l/lagum/lagum.js
@@ -0,0 +1,98 @@
+let musics = [
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "Lagum",
+        singer: "",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+
+    { 
+        name: "",
+        image: "",
+        singer: "Lagum",
+        album: "",
+        link: ""
+    },
+]
